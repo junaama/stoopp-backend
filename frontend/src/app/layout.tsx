20b73d31c89243e4a3b186d720e933920343a478
junaama <0naama0@gmail.com>
2024-08-01 21:06:42 -0400

    migrate to next

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import "./globals.css"
+
+export const metadata: Metadata = {
+    title: 'Full Stack FastAPI Project',
+    description: 'Web site created with Next.js.',
+}
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        <html lang="en">
+            <body>
+                <div id="root">{children}</div>
+            </body>
+        </html>
+
+    )
+}
